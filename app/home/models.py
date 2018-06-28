5171c7b2bc50c5bfe2a0acf9a24895ad9e8f2d06
shaohuazzz <719370718@qq.com>
2018-06-28 16:01:40 +0800

    第五次提交

--- app/home/models.py
+++ app/home/models.py
@@ -8,3 +8,39 @@ class Area(db.Model):
     pingyin = db.Column(db.String(100), nullable=False, index=True)
     key = db.Column(db.String(10))
     is_hot = db.Column(db.Boolean, default=False)
+
+
+class Cinemas(db.Model):
+    __tablename__ = 'cinemas'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100))  # 影院名
+    city = db.Column(db.String(50))  # 所在城市
+    district = db.Column(db.String(50))  # 区域
+    address = db.Column(db.String(200))  # 地址
+    phone = db.Column(db.String(20))  # 电话
+    score = db.Column(db.Float)  # 评分
+    hallnum = db.Column(db.Integer)  # 影厅数量
+    servicecharge = db.Column(db.Float)  # 服务费
+    astrict = db.Column(db.Integer)  # 限购数量
+    flag = db.Column(db.Integer)  # 状态(营业1，休息0)
+    isdelete = db.Column(db.Boolean, default=False)  # 是否删除
+
+class Movies(db.Model):
+    __tablename__ = 'movies'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    showname = db.Column(db.String(100))  #中文名
+    shownameen = db.Column(db.String(200))  #英文名
+    director = db.Column(db.String(100))  # 导演
+    leadingRole = db.Column(db.String(300))  # 主演
+    type = db.Column(db.String(50))  # 类型
+    country = db.Column(db.String(100))  #国家
+    language = db.Column(db.String(50))  #语言
+    duration = db.Column(db.Integer)  #电影时长
+    screeningmodel = db.Column(db.String(20))  #放映模式(2D 3D 4D),可以使用枚举
+    openday = db.Column(db.DateTime)  #上映时间
+    backgroundpicture = db.Column(db.String(100))  #背景图片
+    flag = db.Column(db.Integer)  #状态(热映 即将上映)
+    isdelete = db.Column(db.Boolean,default=False)  #是否删除,默认未删除
+
+
+
--- app/home/views.py
+++ app/home/views.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify
+from sqlalchemy import func
+from sqlalchemy.util import to_list
 
-from app.home.models import Area
+from app.home.models import Area , Movies ,Cinemas
 # from app.utils.json_utils import to_list
 
 home = Blueprint('home', __name__)
@@ -8,4 +10,19 @@ home = Blueprint('home', __name__)
 @home.route('/1/')
 def home111():
     Area.query.all()
+    return '111'
+
+@home.route('/moves/',methods=['POST'])
+def movies():
+    result = {}
+    try:
+        movie = {}
+        counts = Movies.query.with_entities(Movies.flag,func.count('*')).group_by(Movies.flag).all()
+        hot_movies = Movies.query.filter(Movies.flag == 1).limit(5).all()
+        show_movies = Movies.query.filter(Movies.flag == 2).limit(5).all()
+        movie.update(counts=counts,hot_movies=to_list(hot_movies),show_movies=to_list(show_movies))
+        result.update(atatus=200,msg='success',data=movie)
+    except:
+        result.update(atatus=404,msg='fail')
+
     return '111'
